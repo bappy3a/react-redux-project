fea0660c765b627646c4ea70e99dea91aa2b0c68
Ahmed Bappy <bappy3a@gmail.com>
2024-03-10 17:41:51 +0600

    multiple reducer create

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
 function HooksCounter({}) {
-    const count = useSelector((state) => state.value);
+    const count = useSelector((state) => state.counter.value);
     const dispatch = useDispatch();
 
     const incrementHandler = (value) =>{
